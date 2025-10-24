cc6ac745a3e07a17588925765aaf7e3d1f2b1a77
Corey Clark <Clarkcj12@users.noreply.github.com>
2025-10-24 19:22:39 -0400

    feat(components/common): export Button and ButtonProps from common index

--- components/common/index.ts
+++ components/common/index.ts
@@ -9,6 +9,9 @@
 export { Input } from "./Input";
 export type { InputProps } from "./Input";
 
+export { Button } from "./Button";
+export type { ButtonProps } from "./Button";
+
 export { Checkbox } from "./Checkbox";
 
 export { 
